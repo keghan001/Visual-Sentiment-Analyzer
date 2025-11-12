36ecd20fc09328ef82b899aad6016d6bde614cbc
Kwesi Manu Eghan <115375160+keghan001@users.noreply.github.com>
2025-11-12 17:32:06 +0000

    Dataset Loader Init

---
+++ training/meld_data_loader.py
@@ -0,0 +1,10 @@
+from torch.utils.data import Dataset
+
+
+class MELDDataset(Dataset):
+    def __init__(self) -> None:
+        print("hello")
+        
+if __name__ == "__main__":
+    meld = MELDDataset()
+    
