8fd4e1d085eed2fd1189a0273c92463b86801155
Kwesi Manu Eghan <115375160+keghan001@users.noreply.github.com>
2025-11-24 21:28:50 +0000

    Update training script for SageMaker: configure environment variables

--- train_sagemaker.py
+++ train_sagemaker.py
@@ -1,16 +1,20 @@
 from sagemaker.pytorch import PyTorch
 from sagemaker.debugger import TensorBoardOutputConfig
+from dotenv import load_dotenv
+import os
 
 def start_training():
     tsboard_config = TensorBoardOutputConfig(
-        s3_output_path="bucket-name/tensorboard",
+        s3_output_path="s3://sentiment-analyzer-saas/tensorboard",
         container_local_output_path="/opt/ml/output/tensorboard"
     )
+    
+    load_dotenv()
     
     estimator = PyTorch(
         entry_point="train.py",
         source_dir="training",
-        role="my-new-role",
+        role=os.getenv("SG_ROLE"),
         framework_version="2.5.1",
         py_version="py311",
         instance_count=1,
@@ -21,6 +25,14 @@ def start_training():
         },
         tensorboard_config=tsboard_config
     )
+    
+    # Start training
+    estimator.fit({
+        "training": "s3://sentiment-analyzer-saas/dataset/train",
+        "validation": "s3://sentiment-analyzer-saas/dataset/dev",
+        "test": "s3://sentiment-analyzer-saas/dataset/test"
+    })
+
 
 if __name__ == "__main__":
     start_training()
