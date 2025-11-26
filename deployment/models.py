d1deca98eeddd836a7313a2304c15abe70587d6a
Kwesi Manu Eghan <115375160+keghan001@users.noreply.github.com>
2025-11-26 18:40:27 +0000

    Initialize the model deployment dir

---
+++ deployment/models.py
@@ -0,0 +1,185 @@
+import torch
+import torch.nn as nn
+from transformers import BertModel
+from torchvision import models as vision_models
+from pathlib import Path
+
+
+class TextEncoder(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+        
+        #Preventing pretrained model from being trained
+        for param in self.bert.parameters():
+            param.requires_grad = False
+            
+        self.projection = nn.Linear(768, 128)
+    
+    def forward(self, input_ids, attention_mask):
+        #Extract BERT embeddings
+        outputs = self.bert(input_ids= input_ids, attention_mask= attention_mask)
+        
+        #Use [CLS] token representation
+        pooler_out = outputs.pooler_output
+        
+        return self.projection(pooler_out)
+        
+class VideoEncoder(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.backbone = vision_models.video.r3d_18(pretrained=True)
+        
+        #Preventing pretrained model from being trained
+        for param in self.backbone.parameters():
+            param.requires_grad = False
+        
+        num_fts = self.backbone.fc.in_features
+        
+        self.backbone.fc = nn.Sequential( #type: ignore
+            nn.Linear(num_fts, 128),
+            nn.ReLU(),
+            nn.Dropout(0.2)
+        )
+        
+    def forward(self, x):
+        x = x.transpose(1,2)
+        return self.backbone(x)
+
+class AudioEncoder(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv_layers = nn.Sequential(
+            # Lower level features
+            nn.Conv1d(64, 64, kernel_size=3),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.MaxPool1d(2),
+            # Higher level features
+            nn.Conv1d(64, 128, kernel_size=3),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool1d(1)
+            
+        )
+        
+        for param in self.conv_layers.parameters():
+            param.requires_grad = False
+        
+        self.proj_layers = nn.Sequential(
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Dropout(0.2)
+        )
+        
+    def forward(self, x):
+        x = x.squeeze(1)
+        
+        features = self.conv_layers(x)
+        # Features output: [batch_size, 128, 1]
+        
+        return self.proj_layers(features.squeeze(-1))
+
+
+class MultimodalSentimentModel(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        self.text_encoder = TextEncoder()
+        self.video_encoder = VideoEncoder()
+        self.audio_encoder = AudioEncoder()
+        
+        self.fusion_layer = nn.Sequential(
+            nn.Linear(128*3, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(0.3)
+        )
+        
+        self.emotion_classifier = nn.Sequential(
+            nn.Linear(256, 64),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(64, 7)
+        )
+        
+        self.sentiment_classifier = nn.Sequential(
+            nn.Linear(256, 64),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(64, 3) # Negative, Neutral, Positive
+        )
+    
+    def forward(self, text_input, video_frames, audio_features):
+        text_features = self.text_encoder(
+            text_input['input_ids'],
+            text_input['attention_mask']
+        )
+        
+        video_features = self.video_encoder(video_frames)
+        audio_features = self.audio_encoder(audio_features)
+        
+        # Concatenate multimodal features
+        combined_features = torch.cat([
+            text_features,
+            video_features,
+            audio_features
+        ], dim=1) # [batch_size, 128 * 3]      
+        
+        # Fusion layer
+        fused_feats = self.fusion_layer(combined_features)
+        
+        emotion_output = self.emotion_classifier(fused_feats)
+        sentiment_output = self.sentiment_classifier(fused_feats)
+        
+        return {
+            'emotions': emotion_output,
+            'sentiments': sentiment_output
+        }
+        
+def compute_class_weights(dataset):
+    emotion_counts = torch.zeros(7)
+    sentiment_counts = torch.zeros(3)
+    skipped = 0
+    total = len(dataset)
+    
+    print("\nCounting class distributions...")
+    for i in range(total):
+        sample = dataset[i]
+        
+        if sample is None:
+            skipped += 1
+            continue
+        
+        emotion_label = sample['emotion_label']
+        sentiment_label = sample['sentiment_label']
+        
+        emotion_counts[emotion_label] += 1
+        sentiment_counts[sentiment_label] += 1
+        
+    valid = total - skipped
+    print(f"Skipped samples: {skipped}/{total}")
+    
+    print("\nClass distribution")
+    print("Emotions:")
+    emotion_map = {
+        0: 'anger', 1: 'disgust', 2: 'fear', 3: 'joy',
+        4: 'neutral', 5: 'sadness', 6: 'surprise'}
+    for i, count in enumerate(emotion_counts):
+        print(f"{emotion_map[i]}: {count/valid:.2f}")
+    
+    print("Sentiment:")
+    sentiment_map = {
+        0: 'negative', 1: 'neutral', 2: 'positive'}  
+    for i, count in enumerate(sentiment_counts):
+        print(f"{sentiment_map[i]}: {count/valid:.2f}")
+        
+    #Calculate class weights
+    emotion_weights = 1.0 / emotion_counts
+    sentiment_weights = 1.0 / sentiment_counts
+    
+    # Normalizing weights
+    emotion_weights = emotion_weights / emotion_weights.sum()
+    sentiment_weights = sentiment_weights / sentiment_weights.sum()
+    
+    return emotion_weights, sentiment_weights
